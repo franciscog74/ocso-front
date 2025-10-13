6645dd1fb4e9da04c6cb364eeef9d24876447bab
franciscog74 <fragrz00@gmail.com>
2025-10-13 15:36:49 -0600

    fix: locations solo se ve en /dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { usePathname } from "next/navigation";
 import Header from "./_components/Header";
 import Sidebar from "./_components/_sidebar/Sidebar";
 
@@ -8,13 +11,14 @@ export default function DashboardLayout({
     children: React.ReactNode,
     locations: React.ReactNode
 }) {
+    const path = usePathname();
     return (
         <div className="w-screen h-screen bg-orange-50">
             <Header />
             <div className="flex flex-row items-center">
                 <Sidebar />
                 {children}
-                {locations}
+                {(path === "/dashboard") ? locations : null}
             </div>
         </div>
     );
