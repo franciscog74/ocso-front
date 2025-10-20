75fc725577bb73484996714b7ae00b337b7d692e
franciscog74 <fragrz00@gmail.com>
2025-10-20 13:50:49 -0600

    feat: página provider por id

---
+++ app/dashboard/products/[id]/_components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import { Product } from "@/entities";
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+
+export default function ProductCard({ product }: { product: Product }) {
+    return (
+        <Card className="mx-10 my-5 py-2 overflow-visible">
+            <CardHeader>
+                <p className="w-full text-xl text-center"><b>{product.productName}</b></p>
+            </CardHeader>
+            <Divider />
+            <CardBody className=" text-lg">
+                <p className="w-full">Precio: <b>${product.price}</b></p>
+                <p className="w-full">Sellos: <b>{product.sealCount}</b></p>
+            </CardBody>
+        </Card>
+    );
+}
--- app/dashboard/providers/[id]/page.tsx
+++ app/dashboard/providers/[id]/page.tsx
@@ -1,3 +1,37 @@
-export default function ProviderPage() {
-    return null;
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProviderCard from "./_components/ProviderCard";
+import { Provider } from "@/entities";
+import ProductCard from "../../products/[id]/_components/ProductCard";
+import Link from "next/link";
+
+export default async function ProviderPage({ params: { id } }: { params: { id: string } }) {
+    const response = await fetch(`${API_URL}/providers/${id}`, {
+        headers: {
+            ...authHeaders()
+        },
+        next: {
+            tags: ["dashboard:providers", `dashboard:providers:${id}`]
+        }
+    });
+    const provider: Provider = await response.json();
+    return (
+        <div className="flex flex-row h-[90vh] px-10 w-11/12">
+            <div className="w-4/12 self-center">
+                <ProviderCard provider={provider} />
+            </div>
+            <div className="w-8/12 flex flex-col py-5 gap-5 overflow-hidden overflow-y-auto">
+                {provider.products ?
+                    provider.products.map((product) =>(
+                        <Link href={{pathname: `/dashboard/products/${product.productID}`}}
+                            key={product.productID}
+                            className="hover:scale-105 transition-transform">
+                            <ProductCard product={product} />
+                        </Link>
+                    )) :
+                    <p className="text-2xl">No tiene productos</p>
+                }
+            </div>
+        </div>
+    );
 }
