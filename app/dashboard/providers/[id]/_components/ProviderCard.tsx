2dafa2b02814521f75865067b7222933d7a0d3a8
franciscog74 <fragrz00@gmail.com>
2025-10-20 08:35:00 -0600

    chore: eliminar import innecesario en provider card

--- app/dashboard/providers/[id]/_components/ProviderCard.tsx
+++ app/dashboard/providers/[id]/_components/ProviderCard.tsx
@@ -1,6 +1,5 @@
 import { Provider } from "@/entities";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import Link from "next/link";
 
 export default function ProviderCard({ provider }: { provider: Provider }) {
     return (
