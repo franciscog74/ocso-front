7c2ba800bb3da19b60974b78ae66a7e4faee78ba
franciscog74 <fragrz00@gmail.com>
2025-11-01 22:46:31 -0600

    fix: función es constante en vez de variable

--- app/dashboard/products/[id]/_components/FormUpdateProduct.tsx
+++ app/dashboard/products/[id]/_components/FormUpdateProduct.tsx
@@ -5,7 +5,7 @@ import { LuDollarSign } from "react-icons/lu";
 import SelectProvider from "../../_components/SelectProvider";
 
 export default function FormUpdateProduct({ product, providers }: { product: Product, providers: Provider[] }) {
-    let updateWithProductId = updateProduct.bind(null, product.productID);
+    const updateWithProductId = updateProduct.bind(null, product.productID);
     return (
         <form action={updateWithProductId} className="bg-orange-200 py-4 mx-10 px-10 flex flex-col gap-4 w-auto rounded-lg">
             <h1 className="text-center text-2xl">Modificar datos</h1>
